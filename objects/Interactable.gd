702eaeff2055b881330416f5734494282c332a9c
Joney <18376621755@163.com>
2026-01-07 00:28:18 +0800

    feat: 道具拾取

--- objects/Interactable.gd
+++ objects/Interactable.gd
@@ -1,7 +1,10 @@
+tool
 extends Area2D
 class_name Interactable
 
 signal interact
+
+export var texture: Texture setget set_texture
 
 func _input_event(viewport, event, shape_idx):
 	if not event.is_action_pressed("interact"):
@@ -11,3 +14,26 @@ func _input_event(viewport, event, shape_idx):
 
 func _interact():
 	emit_signal("interact")
+
+func set_texture(v: Texture):
+	texture = v
+	
+	#如果发现含有重复添加的 就删掉
+	for node in get_children():
+		if node.owner == null:
+			node.queue_free()
+	
+	#通过程序 + 入新东西
+	if texture == null:
+		return
+	
+	var sprite := Sprite.new()
+	sprite.texture = texture
+	add_child(sprite)
+	
+	var rect := RectangleShape2D.new()
+	rect.extents = v.get_size() /2
+	
+	var collider := CollisionShape2D.new()
+	collider.shape = rect
+	add_child(collider)
---
+++ objects/SceneItem.gd
@@ -0,0 +1,28 @@
+tool
+extends Interactable
+class_name SceneItem
+
+export var item: Resource setget set_item
+
+func set_item(v: Item):
+	item = v
+	set_texture(item.scene_texture if item else null)
+	#3.x Godot 版本的一个bug： 在一个导出变量的set里运用了另一个导出变量的set，
+	#它只能刷新 item 而不是刷新另一个texure 我们需要手动all
+	property_list_changed_notify()
+
+func _interact():
+	._interact()
+	
+	# 一个占位的假消失 这样可以做一个动画出来 直接让本体去动画 可能不安全
+	var sprite := Sprite.new()
+	sprite.texture = item.scene_texture
+	get_parent().add_child(sprite)
+	sprite.global_position = global_position
+	
+	var tween := sprite.create_tween()
+	tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	tween.tween_property(sprite, 'scale', Vector2.ZERO, 0.15)
+	tween.tween_callback(sprite, 'queue_free')
+	
+	queue_free()
